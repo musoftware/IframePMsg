5bde7c954c197d8fb7cce7d45430f96fdc4a2aef
Mahmoud Mu <mahmoudmn810@gmail.com>
2021-03-02 03:33:39 +0200

    update

--- iframeAutoHeight1.3.js
+++ iframeAutoHeight1.3.js
@@ -1,6 +1,6 @@
 var receiveMessage = function(event) {
     if (event.data.startsWith("AutoHeight:V")){
-        let getIframe = document.getElementsByTagName('iframe');
+        let getIframe = document.getElementsByClassName('dynamic_height');
         if (getIframe.length > 0){
             getIframe[0].style.height = event.data.substring("AutoHeight:V".length);
         }
